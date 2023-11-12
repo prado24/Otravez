0f7a8cccd0388fe088cc0ead4c687e3dbbaee91e
prado <pradojimenezmaria@gmail.com>
2023-11-12 11:12:43 -0600

    Se agergo referecias a AccesoTienda

---
+++ PresentacionPrueba/AccesoDatosPrueba/AccesoTienda.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+namespace AccesoDatosPrueba
+{
+    public class AccesoTienda
+    {
+    }
+}
