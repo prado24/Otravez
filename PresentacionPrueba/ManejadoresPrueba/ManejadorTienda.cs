08e016177cb57203dc23a6c6acd91f531428056a
prado <pradojimenezmaria@gmail.com>
2023-11-12 11:04:48 -0600

    Se agrego Manejadores

--- PresentacionPrueba/ManejadoresPrueba/ManejadorTienda.cs
+++ PresentacionPrueba/ManejadoresPrueba/ManejadorTienda.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AccesoDatos
+namespace ManejadoresPrueba
 {
-    public class Tienda
+    public class ManejadorTienda
     {
     }
 }
